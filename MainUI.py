efa7f627207cf643ae84361ce27a98f3cb8c0eae
kyun <annakky@naver.com>
2024-03-14 17:03:25 +0900

    refactor: main window -> widget으로 변경

--- MainUI.py
+++ MainUI.py
@@ -1,50 +1,22 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton, QSizePolicy, QVBoxLayout
 
 from Chart import Chart
 from strategy.Crossover import Crossover
 from strategy.MyStrategy import MyStrategy
 from strategy.RsiRange import RsiRange
+from ui.StrategyWidget import StrategyWidget
 
 form_class = uic.loadUiType("strategy-ui.ui")[0]
 
-class MyMainWindow(QMainWindow, form_class):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+class MyMainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.my_strategy = MyStrategy([], 0)
-        self.chart = Chart(self.my_strategy)
-        self.body_layout.insertWidget(0, self.chart.get_webview())
-
-    def create_strategy(self):
-        strategies = []
-        if self.rsi_checkbox.isChecked():
-            period = self.rsi_period_value.value()
-            low = self.rsi_low_value.value()
-            high = self.rsi_high_value.value()
-            strategies.append(RsiRange(period=period, low=low, high=high))
-
-        if self.crossover_checkbox.isChecked():
-            short = self.crossover_short_value.value()
-            long = self.crossover_long_value.value()
-            strategies.append(Crossover(short, long))
-
-        self.my_strategy = MyStrategy(strategies, self.threshold_value.value())
-
-        self.redraw_chart()
-
-    def set_datetime(self):
-        string_format = 'yyyy-MM-dd hh:mm:ss'
-        self.start = self.start_datetime_value.dateTime().toString(string_format)
-        self.end = self.end_datetime_value.dateTime().toString(string_format)
-
-    def redraw_chart(self):
-        self.set_datetime()
-        self.chart.set_datetime(self.start, self.end)
-        self.chart.set_strategy(self.my_strategy)
-        self.chart.draw()
+        self.strategy_widget = StrategyWidget(self)
+        self.setCentralWidget(self.strategy_widget)
 
 
 if __name__ == "__main__":

58e859a75e0a2b6aadbe4772dca40089b8d5d088
kyun <annakky@naver.com>
2024-04-02 23:49:50 +0900

    refactor: 화면 구성 리팩터링 변경

--- ui/StrategyWidget.py
+++ ui/StrategyWidget.py
@@ -1,11 +1,10 @@
-from PyQt5 import uic, QtWidgets
-from PyQt5.QtWidgets import QWidget, QBoxLayout, QVBoxLayout, QGridLayout, QSizePolicy
-from Chart import Chart
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget
 from strategy.Crossover import Crossover
 from strategy.MyStrategy import MyStrategy
 from strategy.RsiRange import RsiRange
 
-form_class = uic.loadUiType("strategy-ui2.ui")[0]
+form_class = uic.loadUiType("static/strategy-ui2.ui")[0]
 
 class StrategyWidget(QWidget, form_class):
     def __init__(self, parent):
@@ -13,8 +12,6 @@ class StrategyWidget(QWidget, form_class):
         self.setupUi(self)
 
         self.my_strategy = MyStrategy([], 0)
-        self.chart = Chart(self.my_strategy)
-        self.body_layout.insertWidget(0, self.chart.get_webview())
 
     def create_strategy(self):
         strategies = []
@@ -29,24 +26,4 @@ class StrategyWidget(QWidget, form_class):
             long = self.crossover_long_value.value()
             strategies.append(Crossover(short, long))
 
-        self.chart.clear_all()
         self.my_strategy = MyStrategy(strategies, self.threshold_value.value())
-
-        self.redraw_chart()
-
-    def set_datetime(self):
-        string_format = 'yyyy-MM-dd hh:mm:ss'
-        self.start = self.start_datetime_value.dateTime().toString(string_format)
-        self.end = self.end_datetime_value.dateTime().toString(string_format)
-
-    def redraw_chart(self):
-        self.set_datetime()
-        self.chart.set_datetime(self.start, self.end)
-        self.chart.set_strategy(self.my_strategy)
-        self.chart.draw()
-
-    def move_to_strategy_widget(self):
-        pass
-
-    def move_to_backtesting_widget(self):
-        self.parentWidget().setCurrentIndex(1)
